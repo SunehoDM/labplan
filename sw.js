2142a97fc78677a33e14ccc9137f007290e60acd
SunehoDM <sunehocoldplay2002@gmail.com>
2026-03-16 17:05:21 +0530

    fix date timezone bug, dd/mm/yyyy, pwa service worker

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'labplan-v1';
+const ASSETS = [
+  '/labplan/',
+  '/labplan/index.html',
+  '/labplan/manifest.json',
+  '/labplan/icon-192.png',
+  '/labplan/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/labplan/'))));
+});
